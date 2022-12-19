7081a1c56d5fe06e4bd56e222252d990797908ff
Giovanni Capelli <giovacape01@gmail.com>
2022-12-19 18:12:33 +0100

    richieste aggiunte.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 // richiesta KM
 
-
+const Km = prompt('Quanti Km farai con il trenino?');
+console.log(Km);
+console.log(typeof Km);
 //richiesta età
-
+const Età = prompt('quanti anni hai?');
+console.log(Età);
+console.log(typeof Età);
 
 // calcolo del biglietto................prezzo al km=0.21
 
