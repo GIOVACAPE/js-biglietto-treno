42e7427bac18358dfd7e2d1711b1b7adfc1f4b54
Giovanni Capelli <giovacape01@gmail.com>
2022-12-20 10:16:18 +0100

    Prezzo Under 18

--- js/script.js
+++ js/script.js
@@ -3,10 +3,38 @@
 const Km = prompt('Quanti Km farai con il trenino?');
 console.log(Km);
 console.log(typeof Km);
+console.log(isNaN(Km))
+
 //richiesta età
+
 const Età = prompt('quanti anni hai?');
-console.log(Età);
-console.log(typeof Età);
+console.log(Eta);
+console.log(typeof Eta);
+console.log(isNaN(Eta))
+
+
+
+// Verifica delle risposte corrette----------------
+
+if (isNaN(Km) == true || isNaN(age) == treue) {
+    alert('Valori non validi, il programma non calcola il biglietto del trenino')
+}
+else {
+   
+    const basePrice = 0.21 * Km;
+    console.log('basePrice', basePrice, typeof basePrice, isNan(basePrice));
+
+    if (age < 18) {
+
+        const childDiscount = basePrice / 100 * 20; // sconto del 20%
+        const finalPrice = basePrice - childDiscount; // Prezzo finale scontato
+        console.log('childDiscount', childDiscount, typeof childDiscount, isNan(childDiscount));
+        console.log('finalPrice', finalPrice, typeof finalPrice, isNan(finalPrice));
+    }
+
+}
+    
+
 
 // calcolo del biglietto................prezzo al km=0.21
 
